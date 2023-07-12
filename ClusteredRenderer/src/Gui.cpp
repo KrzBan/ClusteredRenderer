db81de58789d5b55158290ec7a5b28c93e957ee4
Krzysztof Bański <krzys0100@wp.pl>
2023-07-12 23:19:19 +0200

    Added dockspace

--- ClusteredRenderer/src/Gui.cpp
+++ ClusteredRenderer/src/Gui.cpp
@@ -6,9 +6,18 @@ Gui::Gui(const Window& window) {
 	ImGuiIO& io = ImGui::GetIO();
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 
 	ImGui::StyleColorsDark();
 	//ImGui::StyleColorsLight();
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
 
 	// Setup Platform/Renderer backends
 	ImGui_ImplGlfw_InitForOpenGL(window.glfwWindow(), true);
@@ -25,6 +34,8 @@ void Gui::NewFrame() const {
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplGlfw_NewFrame();
 	ImGui::NewFrame();
+
+	ImGuiID dockspace_id = ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 }
 
 void Gui::Render(const Window& window) const {
@@ -35,4 +46,13 @@ void Gui::Render(const Window& window) const {
 	
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
+	// Update and Render additional Platform Windows
+	ImGuiIO& io = ImGui::GetIO();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		GLFWwindow* backup_current_context = glfwGetCurrentContext();
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+		glfwMakeContextCurrent(backup_current_context);
+	}
 }
