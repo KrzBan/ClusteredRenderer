2337d4d859efd7c0bb4f1f0620f273926c485ca3
Krzysztof Bański <krzys0100@wp.pl>
2024-03-15 21:12:40 +0100

    Morte Scene serialization

--- ClusteredRenderer/src/SceneSerializer.hpp
+++ ClusteredRenderer/src/SceneSerializer.hpp
@@ -32,5 +32,40 @@ public:
 
 template <class Archive>
 void serialize(Archive& archive, Entity& entity) {
-	
+	SerializeComponent<IDComponent>(archive, entity);
+	SerializeComponent<TagComponent>(archive, entity);
+
+	SerializeComponent(AllComponents{}, archive, entity);
+}
+
+template <class Component, typename Archive>
+void SerializeComponent(Archive& archive, Entity& entity) {
+	if (entity.HasComponent<Component>()) {
+		const auto& component = entity.GetComponent<Component>();
+		archive(component);
+	}
+}
+
+template <typename... Component, class Archive>
+void SerializeComponent(ComponentGroup<Component...>, Archive& archive, Entity& entity) {
+	(SerializeComponent<Component>(archive, entity), ...);
+}
+
+template <class Archive>
+void serialize(Archive& archive, IDComponent& entity) {
+}
+template <class Archive>
+void serialize(Archive& archive, TagComponent& entity) {
+}
+template <class Archive>
+void serialize(Archive& archive, TransformComponent& entity) {
+}
+template <class Archive>
+void serialize(Archive& archive, CameraComponent& entity) {
+}
+template <class Archive>
+void serialize(Archive& archive, NativeScriptComponent& entity) {
+}
+template <class Archive>
+void serialize(Archive& archive, MeshRendererComponent& entity) {
 }
